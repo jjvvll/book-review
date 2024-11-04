01801b84d48557556b0a43ef6a0fd1d1cc64a727
jjvvll <llanetajohnver@gmail.com>
2024-11-04 19:09:33 +0800

    Daily update of task list

--- database/migrations/2024_11_03_074107_create_reviews_table.php
+++ database/migrations/2024_11_03_074107_create_reviews_table.php
@@ -13,9 +13,20 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+
+            //$table->unsignedBigInteger('Book_id');  //foreign key
+
             $table->string('review');
             $table->unsignedTinyInteger('rating');
             $table->timestamps();
+
+            // $table->foreign('book_id')->references('id')->on('books')
+            //     ->onDelete('cascade');
+
+            $table->foreignId('book_id')->constrained()
+            ->cascadeOnDelete();
+
+
         });
     }
 
